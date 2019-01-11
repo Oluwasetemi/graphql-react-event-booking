4ea874ac0ff0b9259a5c4acea644a498ff2103de
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-01-11 23:51:15 +0100

    #7 completed

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/events');
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema');
+const graphQlResolvers = require('./graphql/resolvers');
 
 const app = express();
 
@@ -14,121 +12,8 @@ app.use(bodyParser.json());
 
 // eslint-disable-next-line prettier/prettier
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    type User {
-      _id: ID!
-      email: String!
-      password: String
-    }
-
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    input UserInput {
-      email: String!
-      password: String!
-    }
-
-    type RootQuery {
-      events: [Event!]!
-      user: [User!]!
-    }
-
-    type RootMutation {
-      createEvent(eventInput: EventInput): Event
-      createUser(userInput: UserInput): User
-    }
-
-    schema{
-      query: RootQuery
-      mutation: RootMutation
-    }
-  `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              return { ...event._doc, _id: event.id };
-            });
-          }).catch(err => {
-            throw err;
-          })
-      },
-      createEvent: args => {
-        const { title, description, price, date } = args.eventInput;
-        const event = new Event({
-          title,
-          description,
-          price: +price,
-          date: new Date(date),
-          creator: '5c37ddf3fc64431518dbcc22',
-        });
-        let createdEvent;
-        return event
-          .save()
-          .then(result => {
-            createdEvent = {
-              ...result._doc,
-              _id: result._doc._id.toString(),
-            };
-            return User.findById('5c37ddf3fc64431518dbcc22')
-
-          })
-          .then(user => {
-            if (!user) {
-              throw new Error('User cannot be found.');
-            }
-            user.createdEvents.push(event);
-            return user.save();
-          })
-          .then(result => {
-            return createdEvent;
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createUser: args => {
-        const { email, password } = args.userInput;
-        return User.findOne({ email })
-          .then(user => {
-            if (user) {
-              throw new Error('User exists already.');
-            }
-            return bcrypt.hash(password, 12);
-          })
-          .then(hashedPassword => {
-            const user = new User({
-              email,
-              password: hashedPassword,
-            });
-            return user.save();
-          })
-          .then(result => {
-            return {
-              ...result._doc,
-              password: null,
-              _id: result.id,
-            }
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true,
   })
 );
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-use-before-define */
+const bcrypt = require('bcryptjs');
+
+const Event = require('../../models/events');
+const User = require('../../models/user');
+
+const events = async eventIds => {
+  try {
+    // eslint-disable-next-line no-shadow
+    const events = await Event.find({ _id: { $in: eventIds } });
+    events.map(event => ({
+      ...event._doc,
+      _id: event.id,
+      date: new Date(event._doc.date).toISOString(),
+      // eslint-disable-next-line no-use-before-define
+      creator: user.bind(this, event.creator),
+    }));
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async userId => {
+  try {
+    // eslint-disable-next-line no-shadow
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  events: async () => {
+    try {
+      // eslint-disable-next-line no-shadow
+      const events = await Event.find();
+      return events.map(event => ({
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event._doc.creator),
+      }));
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const { title, description, price, date } = args.eventInput;
+    const event = new Event({
+      title,
+      description,
+      price: +price,
+      date: new Date(date),
+      creator: '5c39160f461f5e4e4ca921e8',
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = {
+        ...result._doc,
+        _id: result._doc._id.toString(),
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, result._doc.creator),
+      };
+      // eslint-disable-next-line no-shadow
+      const creator = await User.findById('5c39160f461f5e4e4ca921e8');
+
+      if (!creator) {
+        throw new Error('User cannot be found.');
+      }
+
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (error) {
+      throw error;
+    }
+  },
+  createUser: async args => {
+    const { email, password } = args.userInput;
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        throw new Error('User exists already.');
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      // eslint-disable-next-line no-shadow
+      const user = new User({
+        email,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      return {
+        ...result._doc,
+        password: null,
+        _id: result.id,
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
